ab0be5515718b072bd0c67a6a0b37d3c4094115d
Oliver Ford  @TacticalGrace <codes20@oliford.co.uk>
2021-07-28 15:39:27 +0200

    Some attempt to develop cylindrical lenses

---
+++ src/fusionOptics/optics/CylindricalPlanarConvexLens.java
@@ -0,0 +1,170 @@
+package fusionOptics.optics;
+
+import net.jafama.FastMath;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import fusionOptics.Util;
+import fusionOptics.interfaces.IsoIsoStdFresnel;
+import fusionOptics.surfaces.Aspheric;
+import fusionOptics.surfaces.Cylinder;
+import fusionOptics.surfaces.Disc;
+import fusionOptics.surfaces.Dish;
+import fusionOptics.surfaces.TruncatedCylinder;
+import fusionOptics.types.Interface;
+import fusionOptics.types.Material;
+import fusionOptics.types.Medium;
+import fusionOptics.types.Optic;
+import fusionOptics.types.Surface;
+import algorithmrepository.exceptions.NotImplementedException;
+
+/** One spherical surface and one flat
+ * 
+ * Convex: +ve RadCurv or focalLength
+ *        ----|
+ *       /    |
+ *      /     |
+ *     /      |
+ *    |       | ------> Normal
+ *    |       |
+ *     \      |    ^
+ *      \     |    | up
+ *       \    |    |
+ *        ----|
+ * 
+ * 
+ * Concave: -ve RadCurv or focalLength
+ *     -------|
+ *     \      |
+ *      \     |
+ *       \    |
+ *        |   | ------> Normal
+ *        |   |
+ *       /    |    ^
+ *      /     |    | up
+ *     /      |    |
+ *     -------|
+ *     
+ *     
+ *     up is the axis along which the surface is curved
+ *     the surface is straight along: right = normal x up
+ * 
+ *  */
+public class CylindricalPlanarConvexLens extends Optic {
+
+	private double centre[];
+	private double radiusOfCurv;
+	private Disc flatSurface;
+	private TruncatedCylinder curvedSurface;
+	private Medium lensMedium;
+	private boolean concave;
+	
+	public final static double focalLengthToRadCurv(double f, double n){
+		return (n - 1.0) * f;
+	}
+		
+	/** Creates a lens with the given focal length for the given wavelength at room temperature.
+	 * Lens has the given edge thickness */
+	public static CylindricalPlanarConvexLens fromFocalLengthAndEdgeThickness(String name, double centreFlat[], double normal[], double up[], double radius, double focalLength, double edgeThickness, double length, Medium lensMedium, Interface iface, double wavelength) {
+				
+		double n = lensMedium.getMaterial().getRefractiveIndex(0, wavelength, 300);
+		
+		double R = focalLengthToRadCurv(focalLength, n);
+
+		return fromRadiusOfCurvAndEdgeThickness(name, centreFlat,  normal, up, radius, R, edgeThickness, length, lensMedium, iface);
+	}
+	
+	/** Creates a lens with the given focal length for the given wavelength at room temperature.
+	 * Lens has the given edge thickness */
+	public static CylindricalPlanarConvexLens fromFocalLengthAndCentreThickness(String name, double centreFlat[], double normal[], double up[], double radius, double focalLength, double centreThickness, double length, Medium lensMedium, Interface iface, double wavelength) {
+		
+		double n = lensMedium.getMaterial().getRefractiveIndex(0, wavelength, 300);
+		
+		double R = (n-1)*focalLength;
+		
+		return new CylindricalPlanarConvexLens(name, centreFlat,  normal, up, radius, R, centreThickness, length, lensMedium, iface);
+	}
+		
+	/** Creates a lens with the given radius of curvature and the given central thickness */
+	public static CylindricalPlanarConvexLens fromRadiusOfCurvAndCentreThickness(String name, double centreFlat[], double normal[], double up[], double radius, double radiusOfCurvature, double centreThickness, double length, Medium lensMedium, Interface iface) {
+		
+		return new CylindricalPlanarConvexLens(name, centreFlat,  normal, up, radius, radiusOfCurvature, centreThickness, length, lensMedium, iface);
+	}
+
+	public static CylindricalPlanarConvexLens fromRadiusOfCurvAndEdgeThickness(String name, double centreFlat[], double normal[], double up[], double radius, double radiusOfCurvature, double edgeThickness, double length, Medium lensMedium, Interface iface) {
+		
+		double centreThickness;
+		if(radiusOfCurvature >= 0){
+			centreThickness = edgeThickness + Dish.depth(radiusOfCurvature, radius);
+		}else{
+			centreThickness = edgeThickness - Dish.depth(-radiusOfCurvature, radius);
+		}
+		return new CylindricalPlanarConvexLens(name, centreFlat, normal, up, radius, radiusOfCurvature, centreThickness, length, lensMedium, iface);
+		
+	}
+	
+	/** Private constructor, use the fromXXX() functions */
+	private CylindricalPlanarConvexLens(String name, double centreFlat[], double normal[], double up[], double radius, double radiusOfCurvature, double centreThickness, double length, Medium lensMedium, Interface iface) {
+		super(name);
+		this.lensMedium = lensMedium;
+
+		
+		double curvedCentre[] = new double[3];
+		double cyldCentre[] = new double[3];
+		double reverseNormal[] = new double[3];
+		centre = new double[3];
+		
+		//dishes have their normal facing toward the centre of their radius of curvature 
+		for(int i=0;i<3;i++){
+			curvedCentre[i] = centreFlat[i] - centreThickness * normal[i];
+			cyldCentre[i] = curvedCentre[i] + radiusOfCurvature * normal[i];
+			reverseNormal[i] = -normal[i];
+			centre[i] = centreFlat[i] - (centreThickness/2.0) * normal[i];
+		}
+		
+		flatSurface = new Disc(getName() + "-flat", centreFlat, normal, radius, null, lensMedium, iface) ;
+		
+		if(radiusOfCurvature >= 0){ //convex
+			this.concave = false;
+			this.radiusOfCurv = radiusOfCurvature;
+			double cyldAxis[] = Util.reNorm(Util.cross(normal, up));
+			curvedSurface = new TruncatedCylinder(getName() + "-curved", cyldCentre, cyldAxis, up, radiusOfCurv, length, 2*radius, lensMedium, null, iface);
+			
+		}else{ //concave
+			this.concave = true;
+			this.radiusOfCurv = -radiusOfCurvature;
+			double cyldAxis[] = Util.reNorm(Util.cross(reverseNormal, up));
+			curvedSurface = new TruncatedCylinder(getName() + "-curved", cyldCentre, cyldAxis, up, radiusOfCurv, length, 2*radius, null, lensMedium, iface);
+		}
+
+		addElement(flatSurface);
+		addElement(curvedSurface);
+	}
+		
+	public static double thickLensFocalLength(double radius1, double radius2, double thickness, double n) {
+		return 1.0 / ((n-1.0)*( 1/radius1 - 1/radius2 + (n-1)*thickness/(n*radius1*radius2) ));
+	}
+
+	public void setFocalLength(double f, double wavelength) {
+		
+		double n= lensMedium.getMaterial().getRefractiveIndex(0, wavelength, 300);
+		double lensRadius = curvedSurface.getDishDiameter()/2;
+		
+		radiusOfCurv = focalLengthToRadCurv(f, n);
+		
+		//frontSurface.setRadiusOfCurv(radiusOfCurv);
+		curvedSurface.setRadiusOfCurv(radiusOfCurv);
+		
+	}
+
+	public TruncatedCylinder getConvexSurface() { return curvedSurface;	}
+	public Disc getPlanarSurface() { return flatSurface;	}
+	
+	public TruncatedCylinder getBackSurface() { return curvedSurface;	}
+	public Disc getFrontSurface() { return flatSurface;	}
+
+	public double getRadius() { return Math.max(flatSurface.getRadius(), curvedSurface.getDishDiameter()/2); }
+	
+}
---
+++ src/fusionOptics/surfaces/TruncatedCylinder.java
@@ -0,0 +1,288 @@
+package fusionOptics.surfaces;
+
+import net.jafama.FastMath;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import fusionOptics.Util;
+import fusionOptics.tracer.Tracer;
+import fusionOptics.types.Interface;
+import fusionOptics.types.Intersection;
+import fusionOptics.types.Medium;
+import fusionOptics.types.RaySegment;
+import fusionOptics.types.Surface;
+
+import algorithmrepository.Algorithms;
+
+
+
+public class TruncatedCylinder extends Surface {
+	
+	private double centre[];
+	private double radius;
+	private double boundRadius;
+	private double length;
+	private double axis[];
+	private double up[];
+	private double right[];
+	/** limits relative to cylinder axis in up and right directions */
+	private double rMin, rMax, uMin, uMax; 
+	
+	/** Drawing properties */
+	private int nPointsPerSection = -1;
+	private int nSections = -1;
+	
+	public TruncatedCylinder(String name, double centre[], double axis[], double up[], double radius, double length, 
+			double height, double width, Interface iface) {
+		this(name, centre, axis, up, radius, length, height, width, null, null, iface);
+	}
+
+	/** right half of cylinder of given height symmetric around horizonal midplane */
+	public TruncatedCylinder(String name, double centre[], double axis[], double up[], double radius, double length, double height, 
+			Medium innerMedium, Medium outerMedium, Interface iface) {
+		this(name, centre, axis, up, radius, length,
+				0, Double.POSITIVE_INFINITY,
+				-height/2,
+				height/2,
+				innerMedium, outerMedium, iface);
+	}
+	
+	public TruncatedCylinder(String name, double centre[], double axis[], double up[], double radius, double length, 
+			double height, double width,
+			Medium innerMedium, Medium outerMedium, Interface iface) {
+		this(name, centre, axis, up, radius, length,
+				-width/2, width/2,
+				-height/2, height/2,
+				innerMedium, outerMedium, iface);
+	}
+
+	public TruncatedCylinder(String name, double centre[], double axis[], double up[], double radius, double length, 
+			double rMin, double rMax, double uMin, double uMax, 
+			Medium innerMedium, Medium outerMedium, Interface iface) {
+		super(name, outerMedium, innerMedium, iface);
+		this.centre =  centre;
+		this.axis = axis;
+		this.length = length;
+		this.radius = radius;
+		this.up = up;
+		this.right = Util.reNorm(Util.cross(axis, up));
+		this.rMin = rMin;
+		this.rMax = rMax;
+		this.uMin = uMin;
+		this.uMax = uMax;
+				
+		this.boundRadius = FastMath.sqrt(radius*radius + length*length/4);
+	}
+
+	@Override
+	public double[] getBoundarySphereCentre() { return centre.clone(); }
+
+	@Override
+	public double getBoundarySphereRadius() { return boundRadius; }	
+	
+	@Override
+	public boolean findEarlierIntersection(RaySegment ray, Intersection hit) {
+		
+		double t[] = Algorithms.cylinderLineIntersection(ray.startPos, ray.dir, centre, axis, radius*radius);
+		
+		if(t.length <= 0)
+			return false;
+		
+		
+		//calc hit positions
+		double P0[] = new double[]{
+				ray.startPos[0] + t[0] * ray.dir[0],
+				ray.startPos[1] + t[0] * ray.dir[1],
+				ray.startPos[2] + t[0] * ray.dir[2],
+		};
+		double P1[] = new double[]{
+				ray.startPos[0] + t[1] * ray.dir[0],
+				ray.startPos[1] + t[1] * ray.dir[1],
+				ray.startPos[2] + t[1] * ray.dir[2],
+		};
+		
+		//calc dist along cylinder axis of each point: (P-C).N
+		double d0 = (P0[0] - centre[0]) * axis[0] + (P0[1] - centre[1]) * axis[1] + (P0[2] - centre[2]) * axis[2]; 
+		double d1 = (P1[0] - centre[0]) * axis[0] + (P1[1] - centre[1]) * axis[1] + (P1[2] - centre[2]) * axis[2];
+		
+		double u0 = (P0[0] - centre[0]) * up[0] + (P0[1] - centre[1]) * up[1] + (P0[2] - centre[2]) * up[2]; 
+		double u1 = (P1[0] - centre[0]) * up[0] + (P1[1] - centre[1]) * up[1] + (P1[2] - centre[2]) * up[2];
+		
+		double r0 = (P0[0] - centre[0]) * right[0] + (P0[1] - centre[1]) * right[1] + (P0[2] - centre[2]) * right[2]; 
+		double r1 = (P1[0] - centre[0]) * right[0] + (P1[1] - centre[1]) * right[1] + (P1[2] - centre[2]) * right[2];
+		
+		boolean lastSurfWasUs = (ray.startHit != null && ray.startHit.surface == this);
+		double reHitTolerance =  lastSurfWasUs ? Tracer.reHitTolerance : 0;
+			
+		//work out if each contact point is on the actual ray and hits within the actual cylinder length
+		boolean p0OnCylinder = t[0] > reHitTolerance 
+								&& d0 >= -length/2 && d0 <= length/2
+								&& u0 >= uMin && u0 < uMax
+								&& r0 >= rMin && r0 < rMax;
+		boolean p1OnCylinder = t[1] > reHitTolerance 
+								&& d1 >= -length/2 && d1 <= length/2
+								&& u1 >= uMin && u1 < uMax
+								&& r1 >= rMin && r1 < rMax;
+		
+		//Intersection hit = new Intersection();
+		double d;
+		boolean hitOnExit;
+		if(p0OnCylinder && (t[0] < t[1] || !p1OnCylinder)){
+			//t[0] is the first or only contact with cylinder
+			if(t[0] > ray.length)
+				return false;
+			
+			hit.pos = P0;
+			ray.length = t[0];
+			d = d0;
+			
+			hitOnExit = t[0] > t[1];
+			
+		}else if(p1OnCylinder && (t[1] < t[0] || !p0OnCylinder)){
+			//t[1] is the first or only contact with cylinder
+			if(t[1] > ray.length)
+				return false;
+			
+			hit.pos = P1;
+			ray.length = t[1];
+			d = d1;
+			
+			hitOnExit = t[1] > t[0];
+		}else{
+			return false;
+		}
+		
+		hit.normal = Util.reNorm(new double[]{ // P - C - d.N
+				hit.pos[0] - centre[0] - d * axis[0],
+				hit.pos[1] - centre[1] - d * axis[1],
+				hit.pos[2] - centre[2] - d * axis[2],
+		});
+		
+		hit.surface = this;
+		return true;
+
+	}
+	
+	public void setDrawingDetails(int nSections, int nPointsPerSection){
+		this.nPointsPerSection = nPointsPerSection;
+		this.nSections = nSections;
+	}
+	
+	@Override
+	public List<double[][]> draw() {
+		int nPointsPerSection = (this.nPointsPerSection > 0) ? this.nPointsPerSection : 5 + (approxDrawQuality * 50 / 100); //min 5, max 55  at def(10)
+		int nSections = (this.nSections > 0) ? this.nSections : 1 + (approxDrawQuality * 1); // min 101, max 33, 11 at def(10)
+		
+		ArrayList<double[][]> lines = new ArrayList<double[][]>();
+		
+		for(int i=0; i < nSections; i++){
+			double phi0 = i * 2 * Math.PI / nSections;
+			double phi1 = (i+1) * 2 * Math.PI / nSections;
+			
+			double dPhi = (phi1 - phi0) / (nPointsPerSection - 1);
+			double line[][] = new double[3][nPointsPerSection*2 + 1];
+			for(int j=0; j < nPointsPerSection; j++){
+				double subPhi = j * dPhi;
+				
+				for(int k=0; k < 3; k++){
+					double u = radius * Math.cos(phi0+subPhi);
+					double r = radius * Math.sin(phi0+subPhi);
+					u = FastMath.max(u, uMin);
+					u = FastMath.min(u, uMax);
+					r = FastMath.max(r, rMin);
+					r = FastMath.min(r, rMax);
+					line[k][j] = centre[k] - length/2 * axis[k]
+				                           + u * up[k]
+				                           + r * right[k];
+				}
+				             
+				for(int k=0; k < 3; k++){
+					double u = radius * Math.cos(phi1-subPhi);
+					double r = radius * Math.sin(phi1-subPhi);
+					u = FastMath.max(u, uMin);
+					u = FastMath.min(u, uMax);
+					r = FastMath.max(r, rMin);
+					r = FastMath.min(r, rMax);
+					line[k][nPointsPerSection+j] = centre[k] + length/2 * axis[k]
+				                           + u * up[k]
+				                           + r * right[k];
+				}
+			
+	
+			}
+
+			line[0][nPointsPerSection*2] = line[0][0];
+			line[1][nPointsPerSection*2] = line[1][0];
+			line[2][nPointsPerSection*2] = line[2][0];
+			
+			lines.add(line);
+		}
+		
+		return lines;
+			
+		
+	}
+
+	@Override
+	public void shift(double[] dX) {
+		centre[0] += dX[0];
+		centre[1] += dX[1];
+		centre[2] += dX[2];
+	}
+
+	@Override
+	public void rotate(double[] point, double[][] matrix) {
+
+		for(int i=0;i<3;i++)
+			centre[i] -= point[i];
+			
+		double newCentre[] = new double[3];
+		double newAxis[] = new double[3];
+		
+		for(int i=0;i<3;i++)
+			for(int j=0;j<3;j++){
+				newCentre[i] += matrix[i][j] * centre[j];
+				newAxis[i] += matrix[i][j] * axis[j];
+			}
+				
+		for(int i=0;i<3;i++)
+			centre[i] = point[i] + newCentre[i];
+		
+		axis = newAxis;
+	}
+
+	public double[] getCentre() { return centre.clone(); }
+	
+	public void setCentre(double[] centre) { this.centre = centre;	}
+	
+	public double[] getAxis() { return axis.clone(); }
+
+	public double getRadius() { return radius; }
+	public double getLength() { return length; }
+	public void setLength(double length){ this.length = length; }
+	public void setRadius(double radius){ this.radius = radius; }
+
+	@Override
+	public int surfaceGeometryHashCode() {
+		long t; int r = 1;
+		r = 31 * r + Arrays.hashCode(axis);
+		r = 31 * r + Arrays.hashCode(centre);
+		t = Double.doubleToLongBits(length);	r = 31 * r + (int) (t ^ (t >>> 32));
+		t = Double.doubleToLongBits(radius); 	r = 31 * r + (int) (t ^ (t >>> 32));
+		return r;
+	}
+
+	@Override
+	public boolean surfaceGeometryEquals(Surface obj) {
+		TruncatedCylinder other = (TruncatedCylinder) obj;
+		return Arrays.equals(axis, other.axis)
+				&& Arrays.equals(centre, other.centre)
+				&& Double.doubleToLongBits(length) == Double.doubleToLongBits(other.length)
+				&& Double.doubleToLongBits(radius) == Double.doubleToLongBits(other.radius);
+	}
+	
+	
+}
