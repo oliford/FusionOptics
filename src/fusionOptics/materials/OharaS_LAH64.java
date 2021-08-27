785c53590a94e330e32c616f4b40f62eb24bf63e
Oliver Ford  @TacticalGrace <codes20@oliford.co.uk>
2021-08-27 19:51:06 +0200

    +Ohara S-LAH64 glass used in ThorLabs's aspheric lenses

---
+++ src/fusionOptics/materials/OharaS_LAH64.java
@@ -0,0 +1,29 @@
+package fusionOptics.materials;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import fusionOptics.types.Material;
+
+public class OharaS_LAH64 extends DispersionCoefficientBased {
+	
+	/** [ https://www.ohara-gmbh.com/fileadmin/user_upload/export-data/pdf/product_datasheets/S-LAH64_English_.pdf ] 
+	 * Coeffs for visible range.  */
+	public static final double coeffs[][] = new double[][]{{
+		1.83021453E+00,		//A1
+		2.91563590E-01,		//A2
+		1.28544024E+00,		//A3
+		9.04823290E-03,		//B1
+		3.30756689E-02,		//B2
+		8.93675501E+01,		//B3
+	}};	
+
+	public OharaS_LAH64() {	super(coeffs); }
+	
+	
+	@Override
+	public double getVerdetConstant(int modeNumber, double wavelen,
+			double temperature) {
+		
+		throw new RuntimeException("Not implemented.");		
+	}
+
+}
