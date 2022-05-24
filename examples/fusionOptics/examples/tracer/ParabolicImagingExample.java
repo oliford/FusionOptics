97cdb6dc64a9fae71144c04a3e972327df2a3fbe
Oliver Ford  @TacticalGrace <codes20@oliford.co.uk>
2022-05-24 16:44:08 +0200

    Parabola pair example, somehow not quite matched and somehow messes up the outgoing rays so that they are short.

--- examples/fusionOptics/examples/tracer/ParabolicImagingExample.java
+++ examples/fusionOptics/examples/tracer/ParabolicImagingExample.java
@@ -5,6 +5,8 @@ import fusionOptics.Util;
 import fusionOptics.drawing.VRMLDrawer;
 import fusionOptics.interfaces.Absorber;
 import fusionOptics.interfaces.IsoIsoStdFresnel;
+import fusionOptics.interfaces.NullInterface;
+import fusionOptics.interfaces.Reflector;
 import fusionOptics.lenses.Nikon50mmF11;
 import fusionOptics.lenses.SchneiderXenon25mmF095;
 import fusionOptics.materials.IsotropicFixedIndexGlass;
@@ -17,7 +19,9 @@ import fusionOptics.pointSpread.PSFGrid;
 import fusionOptics.pointSpread.PointSpreadBuilder;
 import fusionOptics.pointSpread.PointSpreadFunction;
 import fusionOptics.pointSpread.PointsPSF;
+import fusionOptics.surfaces.Disc;
 import fusionOptics.surfaces.Iris;
+import fusionOptics.surfaces.Paraboloid;
 import fusionOptics.surfaces.Square;
 import fusionOptics.tracer.Tracer;
 import fusionOptics.types.Element;
@@ -43,98 +47,102 @@ import oneLiners.OneLiners;
  *
  */
 public class ParabolicImagingExample {
-	final static String outPath = MinervaOpticsSettings.getAppsOutputPath() + "/rayTracing/cameraLensImaging";
+	final static String outPath = MinervaOpticsSettings.getAppsOutputPath() + "/rayTracing/parabolaImaging";
 	
 	final static double maxTheta = 30 * Math.PI / 180;
-	final static int nRaysPerSource = 1000;
+	final static int nRaysPerSource = 10000;
 	final static double rt2 = Math.sqrt(2);
 	
-	final static double gridDef[][] = {
-			{ -0.300, 0.300, 5 }, 
-			{ -0.200, 0.200, 5 }, 
-			{ -0.200, 0.200, 5 }
-		};
-	
-	final static double imageX[] = OneLiners.linSpace(-0.3, 0.3, 500);
-	final static double imageY[] = OneLiners.linSpace(-0.3, 0.3, 500);
+	final static double startY[] = OneLiners.linSpace(-0.3, 0.3, 4);
+	final static double startZ[] = OneLiners.linSpace(-0.3, 0.3, 3);
 	
 	final static double wavelen = 593e-9;
 	
 	public static void main(String[] args) {
 		
-		//SchneiderXenon25mmF095 lens = new SchneiderXenon25mmF095(); double f = 0.025;	//lens focal length
-		Nikon50mmF11 lens = new Nikon50mmF11(); double f = 0.050;	//lens focal length		
+		Square backPlane = new Square("backPlane", new double[]{ -0.3, 0, 0 }, new double[]{ 1, 0, 0 }, new double[]{ 0, 1, 0 }, 10.100, 10.300, Absorber.ideal());
 		
-		double u = 1.000;	//object distance
-		double v = 1.0 / (1.0/f - 1.0/u);	//ideal image distance		
-		Square backPlane = new Square("backPlane", new double[]{ -0.3, 0, 0 }, new double[]{ 1, 0, 0 }, new double[]{ 0, 1, 0 }, 0.100, 0.300, Absorber.ideal());
+		Iris iris1 = new Iris("iris1", new double[] { 2.5, 0, 0}, new double[] {-1,0,0}, 0.5, 0.1, Absorber.ideal());
+		Disc target1 = new Disc("target1", new double[] { 2.5, 0, 0}, new double[] {-1,0,0}, 0.1, NullInterface.ideal());
 		
-		lens.shift(new double[]{ u, 0, 0 });
+		double sepDist = 0.3;
 		
-		Square imgPlane = new Square("imgPlane", new double[]{ u+v, 0, 0 }, new double[]{ -1, 0, 0 }, new double[]{ 0, 1, 0 }, 0.040, 0.040, Absorber.ideal());
+		/* M1 focus at source/dest */
+		double pos[] = { 3.0, 0, 0};
+		double focus[] = { 2.0, 0.0, 0 };
+		double normal[] = {0, 1, 0};
+		//*/
 		
-		PointSpreadBuilder psfBuild = new PointSpreadBuilder(imgPlane, outPath + "/psfData.bin");
-		PSFGrid psfGrid = new PSFGrid("cameraLensImgTestS25mm", gridDef, DualGaussianPSF.class);
+		/* M2 focus at source/dest */
+		double pos2[] = { 3.0, sepDist, 0};
+		double focus2[] = { 2.0, sepDist, 0 };
+		double normal2[] = {0, -1, 0};
+		//*/
 		
-		Optic all = new Optic("all", new Element[]{ backPlane, lens, /*lensIris,*/ imgPlane });
+		/* M1 focus behind other mirror */
+		/*double pos[] = { 3.0, 0, 0 };
+		double focus[] = { 3.0, 1.0, 0 };
+		double normal[] = {-1, 0, 0};
+		//*/
+		
+		/* M2 focus behind other mirror */
+		/*double pos2[] = { 3.0, sepDist, 0};
+		double focus2[] = { 3.0, sepDist - 1.0, 0 };
+		double normal2[] = {-1, 0, 0};
+		//*/
+		
+		Paraboloid paraboloid1 = new Paraboloid("Paraboloid", pos, focus, normal, 0.050, null, null, Reflector.ideal());
+		
+		Paraboloid paraboloid2 = new Paraboloid("Paraboloid", pos2, focus2, normal2, 0.050, null, null, Reflector.ideal());
+		
+		//lens.shift(new double[]{ u, 0, 0 });
+		
+		Square imgPlane = new Square("imgPlane", new double[]{ 2.4, sepDist, 0 }, new double[]{ -1, 0, 0 }, new double[]{ 0, 1, 0 }, 0.400, 0.400, Absorber.ideal());
+		
+		Optic all = new Optic("all", new Element[]{ backPlane, target1, iris1, paraboloid1, paraboloid2, imgPlane });
 		
 		
-		VRMLDrawer vrmlOut = new VRMLDrawer(outPath + "/imgTest.vrml", 0.0001);
+		VRMLDrawer vrmlOut = new VRMLDrawer(outPath + "/imgTest.vrml", 0.5);
 		if(vrmlOut != null) {
 			vrmlOut.setDrawPolarisationFrames(false);
 			vrmlOut.setSkipRays(47);
 		}
 				
 		//double col[][] = ColorMaps.jet(nRaysPerSource);
-		double col[][] = ColorMaps.alternating2D2x2(psfGrid.getNY(), psfGrid.getNX());
+		double col[][] = ColorMaps.alternating2D2x2(startY.length, startZ.length);
 		
 		
-		for(int iZ=0; iZ < psfGrid.getNZ(); iZ++) {
-			for(int iY=0; iY < psfGrid.getNY(); iY++) {
-				for(int iX=0; iX < psfGrid.getNX(); iX++) {
-					double startPos[] = psfGrid.gridPos(iX, iY, iZ);
+		for(int iY=0; iY < startY.length; iY++) {
+			for(int iZ=0; iZ < startZ.length; iZ++) {
+				double startPos[] = new double[] { 0, startY[iY], startZ[iZ] };
+	
+				for(int i=0; i < nRaysPerSource; i++) {
 					
-					psfBuild.startNewPSF(startPos,
-							//new GaussianPSF()
-							new DualGaussianPSF(20)
-							//new PointsPSF()
-							//new MiniImagePSF(20, 20)
-							);
+					RaySegment ray = new RaySegment();
+					ray.startPos = startPos;
+					ray.dir = Tracer.generateRandomRayTowardSurface(ray.startPos, target1);
 					
-					for(int i=0; i < nRaysPerSource; i++) {
-						
-						RaySegment ray = new RaySegment();
-						ray.startPos = startPos;
-						ray.dir = Tracer.generateRandomRayTowardSurface(ray.startPos, lens.getSurfaces().get(0));
-						
-						ray.length = Double.POSITIVE_INFINITY;
-						ray.up = Util.cross(Util.reNorm(Util.cross(ray.dir, new double[]{0,0,1})), ray.dir);
-						
-						
-						ray.E0 = PointSpreadFunction.getInputStatesForMuellerCalc();
-						ray.wavelength = wavelen;
-						
-						Tracer.trace(all, ray, 30, 0.01, true);
-						
-						if(vrmlOut != null)
-							vrmlOut.drawRay(ray, col[iY*psfGrid.getNX()+iX]);
+					ray.length = Double.POSITIVE_INFINITY;
+					ray.up = Util.cross(Util.reNorm(Util.cross(ray.dir, new double[]{0,0,1})), ray.dir);
+					
+					
+					//ray.E0 = PointSpreadFunction.getInputStatesForMuellerCalc();
+					ray.E0 = new double[][]{{1,0,0,0}}; 
+					ray.wavelength = wavelen;
+					
+					Tracer.trace(all, ray, 30, 0.01, true);
+					
+					if(vrmlOut != null)
+						vrmlOut.drawRay(ray, col[iZ*startY.length+iY]);
 
-						ray.processIntersections(imgPlane, psfBuild);
-						psfBuild.nextCoherentSet();
-						
-						Pol.recoverAll();
-					}
+					ray.processIntersections(imgPlane);
 					
-					PointSpreadFunction psf = psfBuild.psfDone(true);
-					psfGrid.put(iX, iY, iZ, psf);
-				
-					System.out.print(".");
+					Pol.recoverAll();
 				}
+				
+				System.out.print(".");
 			}
-			System.out.println("\n" + iZ + ", ");
 		}
-		
-		BinaryMatrixFile.mustWrite(outPath + "/psfAllData.bin", psfGrid.getAllData(), false);
 		
 		Util.throwAwayIrises(all, 100);
 		
